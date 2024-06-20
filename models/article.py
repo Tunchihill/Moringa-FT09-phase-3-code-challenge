bcc1dfa4d6eb941f91f51ceafc6a415123148de7
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-06-20 14:38:54 +0300

    initial commit

--- models/article.py
+++ models/article.py
@@ -1,3 +1,5 @@
+from database.connection import get_db_connection
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
         self.id = id
@@ -6,5 +8,31 @@ class Article:
         self.author_id = author_id
         self.magazine_id = magazine_id
 
+    @property
+    def author(self):
+        from models.author import Author
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM authors WHERE id = ?', (self.author_id,))
+        author_info = cursor.fetchone()
+        conn.close()
+        if author_info:
+            return Author(author_info["id"], author_info["name"])
+        else:
+            return None
+
+    @property
+    def magazine(self):
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM magazines WHERE id = ?', (self.magazine_id,))
+        magazine_info = cursor.fetchone()
+        conn.close()
+        if magazine_info:
+            return Magazine(magazine_info["id"], magazine_info["name"], magazine_info["category"])
+        else:
+            return None
+
     def __repr__(self):
-        return f'<Article {self.title}>'
+        return f'Article(title={self.title}, content={self.content}, author_id={self.author_id}, magazine_id={self.magazine_id})'
